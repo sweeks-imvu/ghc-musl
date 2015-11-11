681610c1d45122ca32f910bd9d168dd97f702e1e
Stephen Weeks <sweeks@imvu.com>
2015-11-10 22:04:18 -0800

    Slight improvement

--- ghc-musl/entrypoint.sh
+++ ghc-musl/entrypoint.sh
@@ -6,7 +6,6 @@
 # to run it under the correct UID/GID.
 #
 set -e
-set -x
 
 # If no command to run specified, just quit.
 [[ $# == 0 ]] && exit 0
@@ -20,10 +19,21 @@ set -x
 # commands in the bind-mounted work directory will be owned by an incorrect UID
 # instead of the user running the commands.
 
-addgroup -g "$WORK_GID" stack
-adduser -D -h "$WORK_HOME" -G stack -u "$WORK_UID" stack
-export PATH="$WORK_HOME/bin:$WORK_HOME/.local/bin:$PATH"
-[[ -d "$WORK_HOME/.cabal" ]] && export PATH="$WORK_HOME/.cabal/bin:$PATH"
-cd "$WORK_WD"
-# Use `sudo` to run the user's command as the correct UID/GID.
-exec sudo -EHu stack PATH="$PATH" /usr/bin/env -- "$@"
+if [[ -n "$WORK_UID" ]] && [[ -n "$WORK_GID" ]] ; then
+  addgroup -g "$WORK_GID" stack
+  adduser -D -h "$WORK_HOME" -G stack -u "$WORK_UID" stack
+  export PATH="$WORK_HOME/bin:$WORK_HOME/.local/bin:$PATH"
+fi
+if [[ -n "$WORK_HOME" ]] ; then
+  [[ -d "$WORK_HOME/.cabal" ]] && export PATH="$WORK_HOME/.cabal/bin:$PATH"
+fi
+if [[ -n "$WORK_WD" ]] ; then
+  cd "$WORK_WD"
+fi
+
+if [[ -n "$WORK_UID" ]] && [[ -n "$WORK_GID" ]] ; then
+  # Use `sudo` to run the user's command as the correct UID/GID.
+  exec sudo -EHu stack PATH="$PATH" /usr/bin/env -- "$@"
+else
+  exec "$@"
+fi
