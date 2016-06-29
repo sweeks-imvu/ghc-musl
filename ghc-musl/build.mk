5ce8d7801f586201c30143d7c042e96a21ec9fbf
Leonidas Loucas <lloucas@imvu.com>
2016-06-28 18:59:41 -0700

    wip to make apline latest still work...

--- ghc-musl/build.mk
+++ ghc-musl/build.mk
@@ -1,6 +1,6 @@
 V = 0
 DYNAMIC_GHC_PROGRAMS = NO
-GhcLibWays = v
+GhcLibWays = $(if $(filter $(DYNAMIC_GHC_PROGRAMS),YES),v dyn,v)
 SRC_HC_OPTS     = -O -H64m -optl--no-pie
 GhcStage1HcOpts = -O -fasm
 GhcStage2HcOpts = -O2 -fasm
